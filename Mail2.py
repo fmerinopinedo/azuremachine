b088151dde766f9ddbb4d98740cfb0741455c11d
fmerinopinedo <109272770+fmerinopinedo@users.noreply.github.com>
2022-07-18 15:38:45 +0200

    Add files via upload

--- Mail2.py
+++ Mail2.py
@@ -1,18 +1,16 @@
 import smtplib, ssl
 
-port = 587  # For starttls
+port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
-sender_email = "merinogrepolis@gmail.com"
-receiver_email = "fernando.merino@servexternos.gruposantander.com"
+sender_email = "merinogrepolis@gmail.com"  # Enter your address
+receiver_email = "fernando.merino@servexternos.gruposantander.com"  # Enter receiver address
 password = "qbeeiuvtsqbrzmuu"
 message = """\
 Subject: Hi there
-This message is sent from Python."""
+
+This message is sent from Python. Prueba"""
 
 context = ssl.create_default_context()
-with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo()  # Can be omitted
-    server.starttls(context=context)
-    server.ehlo()  # Can be omitted
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, message)
