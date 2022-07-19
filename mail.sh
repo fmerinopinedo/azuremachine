f2a4254128ed55bbbf0c625ad3cff584d1f9211e
fmerinopinedo <109272770+fmerinopinedo@users.noreply.github.com>
2022-07-19 16:26:24 +0200

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -1,8 +1,5 @@
-export SMPATH='/home/argus/argus/'
-export PYTHONPATH=$PYTHONPATH:/home/argus
-export PATH='/home/argus/bin:/home/argus/anaconda3/bin:/home/argus/anaconda3/co$'
-
-cd $SMPATH
-
+cd /home/argus/argus
 
 ipython Mail2.py
+
+echo hola
