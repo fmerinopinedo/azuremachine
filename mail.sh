0089c6a20ed2a5e449358edb33c5c8cfe4747bf6
fmerinopinedo <109272770+fmerinopinedo@users.noreply.github.com>
2022-07-19 15:01:09 +0200

    Add files via upload

---
+++ mail.sh
@@ -0,0 +1,8 @@
+export SMPATH='/home/argus/argus/'
+export PYTHONPATH=$PYTHONPATH:/home/argus
+export PATH='/home/argus/bin:/home/argus/anaconda3/bin:/home/argus/anaconda3/co$'
+
+cd $SMPATH
+
+
+python Mail2.py
