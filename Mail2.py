41f97010cf1543065b8358513d5eac4e8bd0ab17
fmerinopinedo <109272770+fmerinopinedo@users.noreply.github.com>
2022-07-20 12:15:15 +0200

    Add files via upload

--- Mail2.py
+++ Mail2.py
@@ -7,44 +7,6 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def sendmails(receiver_emails):
-    for receiver_email in receiver_emails:
-        # Create a multipart message and set headers
-        receiver_email = "fernando.merino@servexternos.gruposantander.com"
-        message = MIMEMultipart()
-        message["From"] = sender_email
-        message["To"] = receiver_email
-        message["Subject"] = subject
-        message["Bcc"] = receiver_email  # Recommended for mass emails
-
-        # Add body to email
-        message.attach(MIMEText(body, "plain"))
-
-        filename = "operacion.xlsx"  # In same directory as script
-
-        # Open PDF file in binary mode
-        with open(filename, "rb") as attachment:
-            # Add file as application/octet-stream
-            # Email client can usually download this automatically as attachment
-            part = MIMEBase("application", "octet-stream")
-            part.set_payload(attachment.read())
-
-        # Encode file in ASCII characters to send by email    
-        encoders.encode_base64(part)
-
-        # Add header as key/value pair to attachment part
-        part.add_header("Content-Disposition","attachment", filename= {filename})
-
-        # Add attachment to message and convert message to string
-        message.attach(part)
-        text = message.as_string()
-
-        # Log in to server using secure context and send email
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, text)
-
 def connect(params_dic):
     """ Connect to the PostgreSQL database server """
     conn = None
@@ -93,6 +55,44 @@ column_names = ["id", "timestamp", "publishedat", "title", "summary", "url", "ur
 df = postgresql_to_dataframe(conn, "select * from argus_dj_articles order by publishedat desc limit 100", column_names)
 df.to_excel("output.xlsx")
 print("Terminado")
+
+def sendmails(receiver_emails):
+    for receiver_email in receiver_emails:
+        # Create a multipart message and set headers
+        receiver_email = "fernando.merino@servexternos.gruposantander.com"
+        message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        message["Subject"] = subject
+        message["Bcc"] = receiver_email  # Recommended for mass emails
+
+        # Add body to email
+        message.attach(MIMEText(body, "plain"))
+
+        filename = "output.xlsx"  # In same directory as script
+
+        # Open PDF file in binary mode
+        with open(filename, "rb") as attachment:
+            # Add file as application/octet-stream
+            # Email client can usually download this automatically as attachment
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+
+        # Encode file in ASCII characters to send by email    
+        encoders.encode_base64(part)
+
+        # Add header as key/value pair to attachment part
+        part.add_header("Content-Disposition","attachment", filename= {filename})
+
+        # Add attachment to message and convert message to string
+        message.attach(part)
+        text = message.as_string()
+
+        # Log in to server using secure context and send email
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, text)
 
 subject = "Informe noticias actualidad"
 body = "Adjunto archivo con las ultimas noticias de actualidad"
